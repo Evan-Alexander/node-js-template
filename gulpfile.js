ac02f1215bebf2bf411c0e15d9c72976ce73af59
Jason Brown and Sean Peterson <jason@>
2017-03-15 08:32:55 -0700

    update json

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var del = require('del');
 var jshint = require('gulp-jshint');
+var buildProduction = utilities.env.production;
+
 var lib = require('bower-files')({
   "overrides":{
     "bootstrap" : {
@@ -19,8 +21,9 @@ var lib = require('bower-files')({
   }
 });
 var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
-var buildProduction = utilities.env.production;
 
 
 
@@ -55,6 +58,7 @@ gulp.task('build', ['clean'], function(){
     gulp.start('jsBrowserify');
   }
   gulp.start('bower');
+  gulp.start('cssBuild');
 });
 
 gulp.task('jshint', function(){
@@ -89,6 +93,7 @@ gulp.task('serve', function() {
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
   gulp.watch(['*.html'], ['htmlBuild']);
+  gulp.watch(['scss/*.scss'], ['cssBuild']);
 });
 
 gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
@@ -101,4 +106,13 @@ gulp.task('bowerBuild', ['bower'], function(){
 
 gulp.task('htmlBuild', function() {
   browserSync.reload();
+});
+
+gulp.task('cssBuild', function() {
+  return gulp.src(['scss/*.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
 });
